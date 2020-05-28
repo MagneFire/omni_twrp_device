0e38015cc2a90f47d4b0d8d6524e6d9faf3861d3
MagneFire <IDaNLContact@gmail.com>
2020-05-28 22:41:40 +0200

    WIP: Touch and theme changes are still needed.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,14 +27,14 @@ TARGET_BOOTLOADER_BOARD_NAME := smelt
 
 # Boot image
 BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=smelt utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup user_debug=31 msm_rtb.filter=0x3
-#BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 BOARD_KERNEL_BASE := 0x0000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x0008000 --ramdisk_offset 0x2000000 --second_offset 0x0f00000 --tags_offset 0x01E00000
 
-TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/zImage-dtb
+#TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/zImage-dtb
 
-#TARGET_KERNEL_SOURCE := kernel
+TARGET_KERNEL_SOURCE := $(DEVICE_TREE)/kernel
 TARGET_KERNEL_CONFIG := smelt_defconfig
 
 # Partitions
@@ -50,14 +50,14 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_HAS_NO_REAL_SDCARD := true
 
-PRODUCT_COPY_FILES += \
-    firmware/image/atmxt-r3.tdat:root/firmware/image/atmxt-r3.tdat \
-    firmware/image/atmxt-r3_hybrid.tdat:root/firmware/image/atmxt-r3_hybrid.tdat \
-    firmware/image/atmxt-r3_large.tdat:root/firmware/image/atmxt-r3_large.tdat \
-    firmware/image/atmxt-r3_small.tdat:root/firmware/image/atmxt-r3_small.tdat \
-    init.recovery.smelt.rc:root/init.recovery.smelt.rc
+#PRODUCT_COPY_FILES += \
+#    firmware/image/atmxt-r3.tdat:root/firmware/image/atmxt-r3.tdat \
+#    firmware/image/atmxt-r3_hybrid.tdat:root/firmware/image/atmxt-r3_hybrid.tdat \
+#    firmware/image/atmxt-r3_large.tdat:root/firmware/image/atmxt-r3_large.tdat \
+#    firmware/image/atmxt-r3_small.tdat:root/firmware/image/atmxt-r3_small.tdat \
+#    init.recovery.smelt.rc:root/init.recovery.smelt.rc
 
-TARGET_RECOVERY_FSTAB := twrp.fstab
+#TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
 
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 
@@ -76,8 +76,9 @@ TWRP_INCLUDE_LOGCAT := true
 TW_USE_NEW_MINADBD := true
 TW_NEW_ION_HEAP := true
 TW_TARGET_USES_QCOM_BSP := true
-#TW_CUSTOM_THEME := watch_mdpi
-TW_ROUND_SCREEN := true
+TW_THEME := watch_mdpi
+#TW_CUSTOM_THEME := $(DEVICE_TREE)/watch_mdpi
+#TW_ROUND_SCREEN := true
 TW_NO_USB_STORAGE := true
 TW_NO_EXFAT := true
 TW_NO_EXFAT_FUSE := true
@@ -88,3 +89,12 @@ TW_INCLUDE_CRYPTO_FBE := false
 TW_EXCLUDE_MTP := true
 TW_EXCLUDE_SUPERSU := true
 ARCH_ARM_HAVE_NEON := true
+TW_WHITELIST_INPUT := atmxt-i2c
+
+#DEVICE_RESOLUTION := 240x240
+TW_X_OFFSET := 40
+TW_Y_OFFSET := 40
+TW_H_OFFSET := 10
+TW_W_OFFSET := 10
+#TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TWRP_EVENT_LOGGING := true
